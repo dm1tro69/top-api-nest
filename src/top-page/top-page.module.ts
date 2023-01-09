f40673c30c4935854a3f233f85a81bea8ce6594c
Dmytro Voronov <dimolg22@gmail.com>
2023-01-09 11:20:09 +0200

    add configModule

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TopPageController } from './top-page.controller';
+import { ConfigModule } from "@nestjs/config";
 
-@Module({})
+
+
+@Module({
+  imports: [ConfigModule],
+  controllers: [TopPageController]
+})
 export class TopPageModule {}
